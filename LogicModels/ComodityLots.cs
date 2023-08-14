c44149d433c4abe0b612aa815d47824390a8755e
Manish Raj SIngh <manishrajsingh@live.in>
2023-08-15 00:25:22 +0530

    _Two

--- LogicModels/ComodityLots.cs
+++ LogicModels/ComodityLots.cs
@@ -10,14 +10,18 @@ namespace MPurchase.LogicModels
     /// Manish Raj Singh@082023
     /// manishrajsingh@live.in
     /// </summary>
+    /// public modifire may replace with internal and private;
     public class ComodityPurchase : IPurchase
     {
         public int PurchaseNo { get; set; }
         public string PurchaseTrno { get; set; }
         public DateTime PrchaseDate { get; set; }
         public string PurchaseName { get; set; }//Required for tagging,grouping and UI.
-       
-     
+        public string PartyName { get; set; }
+        public string BrokerName { get; set; }
+
+        public List<PurchaseLot> Lots = new List<PurchaseLot>();
+
     }
     public class PurchaseLot
     {
@@ -25,24 +29,38 @@ namespace MPurchase.LogicModels
         public int LotNo { get; set; }//To Extend Application to the lab reports...
         public string LotName { get; set; }
         public string Sauda { get; set; }
-        
+        public LotCondition Condition { get; set; }
+        public LotBill Bill { get; set; }
+
         //A Transport may have load of different kinds of objects with them..
         //there is an extention:  Different brokers and parties can be seen in reports...
         //While Transportation was single..and they are devided into lots..
 
-        public string PartyName { get; set; }
-        public string BrokerName { get; set; }
+        //public string PartyName { get; set; }
+        //public string BrokerName { get; set; }
+        //public PurchaseCondition Condition { get; set; }
     }
-    public class PurchaseBill
+    public class LotBill
     {
         public int itemId { get; set; }
         public string ItemName { get; set; }
-        public decimal SaudaRate { get; set; }
+        public decimal SaudaRate { get; set; } = 00M;
         public decimal BillWeight { get; set; }
-        public decimal Rate { get; set; }
-        public decimal Amount { get; set; }
-        public decimal CurrentRate { get; set; }
-        public int  Bags { get; set; }
+        public decimal Rate { get; set; } = 00M;
+        public decimal Amount { get; set; } = 00M;
+        public decimal CurrentRate { get; set; } = 00M;
+        public int Bags { get; set; }
+        public decimal OtherAmount { get; set; }
+        public decimal GstAmount { get; set; } = 00M;
+        public decimal BillAmount { get; set; } = 00M;
     }
+    public class LotCondition
+    {
+        public decimal Oil { get; set; } = 00M;
+        public decimal Ffa { get; set; } = 00M;
+    }
+    public class PurchaseTransport
+    {
 
+    }
 }
